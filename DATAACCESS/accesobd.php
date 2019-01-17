2470d457cedd18b737c64a3933cef04390d3fdcb
uheredero001 <unai.here@gmail.com>
2019-01-17 13:36:39 +0100

    Avances 17/01/2019 (13:30 horas)

--- DATAACCESS/accesobd.php
+++ DATAACCESS/accesobd.php
@@ -33,6 +33,7 @@
     }
 
 
+
     function esAdmin($user)//Devuelve true si el $user es administrador
     {
       global $conexionbd;
@@ -51,6 +52,88 @@
       return false;
     }
 
+    function getUserinfo($nick)//Devuelve un array con todos los datos
+    {
+      global $conexionbd;
+
+      $query = "SELECT * FROM Usuario WHERE Nick='$nick'";
+      $resultado=$conexionbd->query($query);
+      $fila=mysqli_fetch_row($resultado);
+      return $fila;
+    }
+
+
+    function existVehicle ($matricula) //Devuelve 0 si no existe y /=0 si existe
+    {
+      global $conexionbd;
+
+      $query = "SELECT Matricula FROM Vehiculo WHERE Matricula='$matricula'";
+      $resultado=$conexionbd->query($query);
+      return mysqli_num_rows($resultado);
+    }
+
+
+    function getVehicleInfo($matricula)//Devuelve un array con todos los datos
+    {
+      global $conexionbd;
+
+      $query = "SELECT * FROM Vehiculo WHERE Matricula='$matricula'";
+      $resultado=$conexionbd->query($query);
+      $fila=mysqli_fetch_row($resultado);
+      return $fila;
+    }
+
+
+    function existWorker ($dni) //Devuelve 0 si no existe y /=0 si existe
+    {
+      global $conexionbd;
+
+      $query = "SELECT Dni FROM Trabajador WHERE Dni='$dni'";
+      $resultado=$conexionbd->query($query);
+      return mysqli_num_rows($resultado);
+    }
+
+
+    function getWorkerInfo($dni)//Devuelve un array con todos los datos
+    {
+      global $conexionbd;
+
+      $query = "SELECT * FROM Trabajador WHERE Dni='$dni'";
+      $resultado=$conexionbd->query($query);
+      $fila=mysqli_fetch_row($resultado);
+      return $fila;
+    }
+
+
+    function existLugarDescarga ($nombre,$direccion) //Devuelve 0 si no existe y /=0 si existe
+    {
+      global $conexionbd;
+
+      $query = "SELECT Nombre FROM LugarDescarga WHERE Nombre='$nombre' AND Direccion='$direccion'";
+      $resultado=$conexionbd->query($query);
+      return mysqli_num_rows($resultado);
+    }
+
+
+    function getLugarDescargaInfo($dni,$direccion)//Devuelve un array con todos los datos
+    {
+      global $conexionbd;
+
+      $query = "SELECT * FROM LugarDescarga WHERE Nombre='$nombre' AND Direccion='$direccion'";
+      $resultado=$conexionbd->query($query);
+      $fila=mysqli_fetch_row($resultado);
+      return $fila;
+    }
+
+
+    function existViajePredeterminado ($id) //Devuelve 0 si no existe y /=0 si existe
+    {
+      global $conexionbd;
+
+      $query = "SELECT * FROM ViajePredeterminado WHERE Id='$id'";
+      $resultado=$conexionbd->query($query);
+      return mysqli_num_rows($resultado);
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
